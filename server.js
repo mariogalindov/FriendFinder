bcc09b28c252e3df27b968bc41d9f1fd78767da4
Mario Galindo <mariogalindo89@gmail.com>
2019-12-08 20:23:09 -0600

    added content to the public files and routes

--- server.js
+++ server.js
@@ -5,10 +5,8 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-app.get("/", function(req, res) {
-    // res.send("Hello Creep");
-    res.sendFile(path.join(__dirname + "/app/public/home.html"))
-});
+require("./app/routing/htmlRoutes.js") (app);
+require("./app/routing/apiRoutes.js") (app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: "+ PORT);
